17bd900607677ded3bc08cdd18833c646621f13a
1harold0 <harolddiaz.139@gmail.com>
2025-05-15 10:40:21 -0500

    .

--- src/pages/chapterEdit.jsx
+++ src/pages/chapterEdit.jsx
@@ -1,17 +1,19 @@
 import EditChapter from "../components/edithChapter.jsx";
-import Naruto from "../../public/assets/Naruto2.jpg"
+import Naruto from "../../public/assets/Naruto2.jpg";
+
 export default function ChapterEdit() {
     return (
-        <section className="flex min-h-screen">
-         
-            <div className="w-1/2 flex items-center justify-center p-8">
+        <section className="flex min-h-screen flex-col md:flex-row">
+            
+            <div className="w-full md:w-1/2 flex items-center justify-center p-8">
                 <EditChapter/>
             </div>
             
-            <div className="w-1/2 flex items-center justify-center">
+           
+            <div className="hidden md:flex md:w-1/2 items-center justify-center">
                 <img 
                     src={Naruto} 
-                    alt="Descripción de la imagen"
+                    alt="Naruto"
                     className="max-w-full max-h-full object-cover"
                 />
             </div>
