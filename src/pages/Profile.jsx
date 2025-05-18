86c29b6f9b6e145cd4765253de1a51c02483bc29
Elian Garcia <telljua90@gmail.com>
2025-05-18 03:39:04 -0600

    company o author

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -14,15 +14,15 @@ export default function Profile() {
       }}
     >
       {/* Titulo Principal */}
-      <h1 className="text-2xl sm:text-3xl md:text-5xl font-bold leading-tight z-10">
+      <h1 className="text-2xl mb-60 sm:text-3xl md:text-5xl font-bold leading-tight z-10">
         Profile
       </h1>
 
       {/* cuadro blanco */}
       <div
         className="absolute left-1/2 transform -translate-x-1/2
-        bg-white w-[95%] sm:w-11/12 md:w-3/4 z-20 rounded-xl shadow-2xl px-4 sm:px-10 md:px-20 pb-10"
-        style={{ top: "75%" }}
+        bg-white w-[95%] sm:w-11/12 md:w-5/6 z-20 rounded-xl shadow-2xl px-4 sm:px-10 md:px-20 pb-10"
+        style={{ top: "45%" }}
       >
         {/* Informacion dentro del div blanco */}
         <div className="flex justify-center items-center w-full h-full">
