ee7c34a05642423b81acc5faa838970b29df7690
SebastianMayorgaa <jsebastianrmy@gmail.com>
2025-05-19 01:53:02 -0600

    Pagina de Rankings

---
+++ src/components/ranking/RankingNavbar.jsx
@@ -0,0 +1,21 @@
+const routes = [
+    {path: "#popular-mangas", name: "Popular Mangas"},
+    {path: "#popular-mangas-by-author", name: "Popular Mangas By Author"},
+    {path: "#popular-mangas-by-category", name: "Popular Mangas By Category"}
+]
+
+export default function RankingNavbar() {
+
+    return(
+        <div className="px-10 mt-20">
+            {/* fixed top-35 right-0 left-0 z-50 */}
+        <ul className="w-full flex justify-center border-white/75 border-t border-b "> 
+          <li id="Content-Navbar" className="w-[60%] p-5 grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-3 gap-4 text-center">
+            {routes.map((route) => (
+              <a className="text-xl text-white hover:underline" key={route.path} href={route.path}>{route.name}</a>  
+            ))}   
+          </li>
+        </ul>
+        </div>
+    )
+}
